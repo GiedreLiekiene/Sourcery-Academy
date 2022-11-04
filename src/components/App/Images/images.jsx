922910c823783f8148908493a998cb134b34907e
Auksely <auksely@gmail.com>
2022-11-04 16:56:17 +0200

    Keisti tie SVG

---
+++ src/components/App/Images/images.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from './images.js';
+
+export default function Images() {
+  return (
+    <div>
+      <Image.Group303 />
+      <Image.Group351 />
+      <Image.Group363 />
+      <Image.Group365 />
+      <Image.Group406 />
+    </div>
+  );
+}
