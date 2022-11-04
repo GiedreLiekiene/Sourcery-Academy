67365a93357189015fb97d32c606f80c9e27340b
Auksely <auksely@gmail.com>
2022-11-04 17:05:15 +0200

    SVG

--- src/components/App/Images/images.js
+++ src/components/App/Images/images.js
@@ -4,7 +4,6 @@ import { ReactComponent as Group363 } from '/src/assets/svg/Group_303.svg';
 import { ReactComponent as Group365 } from '/src/assets/svg/Group_303.svg';
 import { ReactComponent as Group406 } from '/src/assets/svg/Group_303.svg';
 
-
 export default {
   Group303,
   Group351,
