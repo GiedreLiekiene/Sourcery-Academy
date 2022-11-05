64edb955a3e8566a7c461f7fd02bd3353915f7f2
Linas Paulauskas <paulauskas.linas@gmail.com>
2022-11-05 13:53:53 +0200

    change fragment and main tag to single div with children prop in lyout.js

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './layout.scss';
+import classNames from 'classnames/bind';
+import styles from './layout.scss';
+
+const cn = classNames.bind(styles);
 
 export default function Layout({ children }) {
-  return (
-    <>
-      <main>{children}</main>
-    </>
-  );
+  return <div className={cn('main')}>{children}</div>;
 }
 
 Layout.propTypes = {
