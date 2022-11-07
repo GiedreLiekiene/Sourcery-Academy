35f0f4b275464c67f8613df603b6b743af34ef73
Arturito <arturaskisielius91@gmail.com>
2022-11-07 23:54:52 +0200

    Academies section layout, btns, img, text, styles.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,27 +1,10 @@
 import React from 'react';
-import classNames from 'classnames/bind';
-import PropTypes from 'prop-types';
-import styles from './styles.module';
-import Svg from '~/assets/svg/image.svg';
-import Academies from './Academies/Academies';
+import Academies from './Academies/Academies.js';
 
-const cn = classNames.bind(styles);
-
-export default function App({ prop }) {
-  const charCount = prop.length;
-
+export default function App() {
   return (
-    <div className={cn('wrapper')}>
-      <div className={cn('content')}>
-        <Svg className={cn('image')} />
-        <pre className={cn('helloworld')} style={{ '--count': charCount }}>
-          {prop}
-        </pre>
-      </div>
+    <div>
+      <Academies />
     </div>
   );
 }
-
-App.propTypes = {
-  prop: PropTypes.string,
-};
