7341e656a721bb15d57471b6e0af96485a36dca8
Auksely <auksely@gmail.com>
2022-11-09 15:41:05 +0200

    changed

--- src/assets/svg/assets.js
+++ src/assets/svg/assets.js
@@ -1,5 +1,5 @@
-import { ReactComponent as Group_303 } from "./Group_303.svg";
+import { ReactComponent as Group_303 } from './Group_303.svg';
 
-export const assets ={
-    Group_303,
-}
+export const assets = {
+  Group_303,
+};
