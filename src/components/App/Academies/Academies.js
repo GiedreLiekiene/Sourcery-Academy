e12214c98cc8833c50ca906684d1a39ddae29c62
Arturito <arturaskisielius91@gmail.com>
2022-11-09 22:51:00 +0200

    Styling and button changes due to PR comments

--- src/components/App/Academies/Academies.js
+++ src/components/App/Academies/Academies.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import '../../../sass/layout/academies.scss';
-import Arrow from '../../../assets/svg/Academies/Btn-arrow.svg';
+import ArrowButton from '../../../assets/svg/Academies/Arrow-button.svg';
 import Particles from '../../../assets/svg/Academies/Particles.svg';
 
 export default function Academies() {
   return (
     <div className="container">
-      <div className="row">
-        <Particles className="particles" />
-        <h1>Academies</h1>
-        <p>
-          There are four disciplines available: for developers, testers,
-          front-end developers and kids. Academies are taking place in Devbridge
-          Lithuanian offices. Students, who are willing to join developers,
-          testers or Front-End academies, need to pass the test, prove their
-          best to get an invitation to enroll. This rule doesn’t apply to the
-          kids (7 to 12 years old) academy, the admission is limited only by
-          available number of entries.
-        </p>
-        <span className="vertical-line"></span>
-        <button className="btn-arrow">
-          <Arrow className="arrow" />
-        </button>
-      </div>
+      <Particles className="particles" />
+      <h1>Academies</h1>
+      <p>
+        There are four disciplines available: for developers, testers, front-end
+        developers and kids. Academies are taking place in Devbridge Lithuanian
+        offices. Students, who are willing to join developers, testers or
+        Front-End academies, need to pass the test, prove their best to get an
+        invitation to enroll. This rule doesn’t apply to the kids (7 to 12 years
+        old) academy, the admission is limited only by available number of
+        entries.
+      </p>
+      <span className="vertical-line"></span>
+      <button className="btn-arrow">
+        <ArrowButton />
+      </button>
     </div>
   );
 }
