c4852c3c8c52430b2e62d04e42064ba529e4f909
Arturito <arturaskisielius91@gmail.com>
2022-11-13 18:32:09 +0200

    index.js changes to avoid conflict

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
+import Layout from '../layout/Layout';
 
 export default function App() {
-  return <div></div>;
+  return <Layout></Layout>;
 }
+
+App.propTypes = {
+  prop: PropTypes.string,
+};
