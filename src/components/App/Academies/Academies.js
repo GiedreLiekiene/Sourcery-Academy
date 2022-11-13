bc95f47f8b2e1bf779ffdd44c25ca8a00caf36c8
Arturito <arturaskisielius91@gmail.com>
2022-11-13 18:27:09 +0200

    Style changes acc to notes.

--- src/components/App/Academies/Academies.js
+++ src/components/App/Academies/Academies.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import '../../../sass/layout/academies.scss';
-import ArrowButton from '../../../assets/svg/Academies/Arrow-button.svg';
+import RoundedArrow from '../../../assets/svg/Academies/Rounded-arrow.svg';
 import Particles from '../../../assets/svg/Academies/Particles.svg';
 
 export default function Academies() {
   return (
     <div className="container">
-      <Particles className="particles" />
-      <h1>Academies</h1>
-      <p>
-        There are four disciplines available: for developers, testers, front-end
-        developers and kids. Academies are taking place in Devbridge Lithuanian
-        offices. Students, who are willing to join developers, testers or
-        Front-End academies, need to pass the test, prove their best to get an
-        invitation to enroll. This rule doesn’t apply to the kids (7 to 12 years
-        old) academy, the admission is limited only by available number of
-        entries.
-      </p>
-      <span className="vertical-line"></span>
-      <button className="btn-arrow">
-        <ArrowButton />
-      </button>
+      <Particles className="container__particles" />
+      <div className="container__text">
+        <h1>Academies</h1>
+        <p>
+          There are four disciplines available: for developers, testers,
+          front-end developers and kids. Academies are taking place in Devbridge
+          Lithuanian offices. Students, who are willing to join developers,
+          testers or Front-End academies, need to pass the test, prove their
+          best to get an invitation to enroll. This rule doesn’t apply to the
+          kids (7 to 12 years old) academy, the admission is limited only by
+          available number of entries.
+        </p>
+        <span className="container__vertical-line"></span>
+        <button className="container__rounded-arrow">
+          <RoundedArrow />
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import Academies from './Academies/Academies.js';
 
 export default function App() {
-  return (
-    <div>
-      <Academies />
-    </div>
-  );
+  return <div></div>;
 }
