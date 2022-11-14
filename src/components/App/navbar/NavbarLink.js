a40878d0b44062684338b27cf53d7dec8376903e
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-11-14 21:35:52 +0200

    SR22AS-14 remove redundant onclick & 'active' item

--- src/components/App/navbar/NavbarLink.js
+++ src/components/App/navbar/NavbarLink.js
@@ -5,21 +5,19 @@ import './navbar.scss';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 
-const NavbarLink = ({ link: { path, name, Icon, onClick, submenu } }) => {
+const NavbarLink = ({ link: { path, name, Icon, submenu } }) => {
   const [dropdown, setDropdown] = useState(false);
   return (
     <div>
       <Link
-        className={
-          'navbar__links--link' + (location.pathname === path ? ' active' : '')
-        }
+        className="navbar__links--link"
         to={path}
         key={name}
         aria-expanded={dropdown ? 'true' : 'false'}
         onClick={() => setDropdown((prev) => !prev)}
       >
         {name}
-        {Icon && <Icon className={'navbar__links--link-icon'} />}
+        {Icon && <Icon className="navbar__links--link-icon" />}
       </Link>
       {submenu && <Dropdown submenuItems={submenu} dropdown={dropdown} />}
     </div>
