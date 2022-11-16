e64a32998e71be25bfbb4349b47e9c944ce02f23
Arturito <arturaskisielius91@gmail.com>
2022-11-16 16:28:38 +0200

    Testimonials card component and styling

---
+++ src/components/App/Testimonials/TestimonialsCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../Testimonials/TestimonialsCard.scss';
+import PropTypes from 'prop-types';
+
+function TestimonialsCard({ item }) {
+  return (
+    <div className="card">
+      <div className="card__img">
+        <img src={item.photo} alt="photo" />
+      </div>
+      <div className="card__text">
+        <span className="backticks">“</span>
+        <p>{item.message}</p>
+        <span className="backticks backticks--rotated">“</span>
+      </div>
+      <div className="card__author">
+        <h3>{item.name}</h3>
+        <p>{item.academy}</p>
+      </div>
+    </div>
+  );
+}
+
+TestimonialsCard.propTypes = {
+  item: PropTypes.object,
+};
+
+export default TestimonialsCard;
