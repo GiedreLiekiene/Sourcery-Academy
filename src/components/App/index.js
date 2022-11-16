94df7ea06d9acaf233c3cd7cffbe6eb73d62aede
Paulina Bauraite <bve.paulina@gmail.com>
2022-11-16 15:15:20 +0000

    SR22AS-15 delete not needed lines from index.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,17 +3,6 @@ import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 import styles from './styles.module';
 import Svg from '~/assets/svg/image.svg';
-import { ReactComponent as Lines } from '/src/assets/svg/background-waves/Lines.svg';
-
-const WavesSvg = () => {
-  return (
-    <div className="svg-waves">
-      <Lines />
-    </div>
-  );
-};
-
-export { WavesSvg };
 
 const cn = classNames.bind(styles);
 
