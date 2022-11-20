737153871a56891afb60b6abc699b2ba5a0f3e5c
Linas Paulauskas <paulauskas.linas@gmail.com>
2022-11-20 10:57:02 +0200

    SR22AS-20 add component structure and layout styling

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './card-container.scss';
 
-export default function CardContainer() {
-  return <section></section>;
+export default function CardContainer({ title, alignment, children }) {
+  return (
+    <section className="card-container">
+      <h3
+        className={`card-container__title card-container__title--${alignment}`}
+      >
+        {title}
+      </h3>
+      <div className="card-container__content">{children}</div>
+    </section>
+  );
 }
+
+CardContainer.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+  alignment: PropTypes.string,
+};
