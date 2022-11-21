f4bba1832bd6ad21f3ad860f6f739da9ae5c46bd
Arturito <arturaskisielius91@gmail.com>
2022-11-22 00:13:42 +0200

    Changes made according to notes

--- src/components/Testimonials_card/TestimonialsCard.js
+++ src/components/Testimonials_card/TestimonialsCard.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import '../Testimonials_card/TestimonialsCard.scss';
 import PropTypes from 'prop-types';
+import Backticks from './Backticks';
 
-function TestimonialsCard({ item }) {
+function TestimonialsCard({ item: { name, photo, message, academy } }) {
   return (
-    <div className="card">
-      <div className="card__img">
-        <img src={item.photo} alt="photo" />
+    <div className="testimonials-card">
+      <div className="testimonials-card__img">
+        <img
+          src={photo}
+          alt="Person profile photo"
+          className="testimonials-card__photo"
+        />
       </div>
-      <div className="card__text">
-        <span className="backticks">“</span>
-        <p>{item.message}</p>
-        <span className="backticks backticks--rotated">“</span>
+      <div className="testimonials-card__review">
+        <Backticks />
+        <span className="testimonials-card__text">{message}</span>
       </div>
-      <div className="card__author">
-        <h3>{item.name}</h3>
-        <p>{item.academy}</p>
+      <div className="testimonials-card__reviewer">
+        <span className="testimonials-card__name">{name}</span>
+        <span className="testimonials-card__academy">{academy}</span>
       </div>
     </div>
   );
 }
 
 TestimonialsCard.propTypes = {
-  item: PropTypes.object,
+  item:
+    PropTypes.object &&
+    PropTypes.shape({
+      name: PropTypes.string,
+      photo: PropTypes.string,
+      message: PropTypes.string,
+      academy: PropTypes.string,
+    }),
 };
 
 export default TestimonialsCard;
