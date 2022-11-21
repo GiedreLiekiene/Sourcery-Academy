03becc9725de8f3ff227bd96750d696648d0c552
Arturito <arturaskisielius91@gmail.com>
2022-11-22 00:13:42 +0200

    Changes made according to notes

---
+++ src/components/Testimonials_card/Backticks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Backticks() {
+  return (
+    <div className="backticks">
+      <div className="backticks__start">“</div>
+      <div className="backticks__end">“</div>
+    </div>
+  );
+}
+
+export default Backticks;
