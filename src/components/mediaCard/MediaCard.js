d744faebeb94335c0198b7784c96fa9f64239ae4
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-11-23 20:34:54 +0200

    SR22AS-22 add modifier class --extended

--- src/components/mediaCard/MediaCard.js
+++ src/components/mediaCard/MediaCard.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './media-card.scss';
 import PropTypes from 'prop-types';
 
-const MediaCard = ({ photo, altInfo, doubleWidth }) => {
+const MediaCard = ({ photo, altInfo, extended }) => {
   return (
     <img
       src={photo}
       alt={altInfo}
-      className={'media-card' + (doubleWidth ? ' double-width' : '')}
+      className={'media-card' + (extended ? ' media-card--extended' : '')}
     />
   );
 };
 
 MediaCard.propTypes = {
   photo: PropTypes.string.isRequired,
-  doubleWidth: PropTypes.bool,
+  extended: PropTypes.bool,
   altInfo: PropTypes.string,
 };
 
