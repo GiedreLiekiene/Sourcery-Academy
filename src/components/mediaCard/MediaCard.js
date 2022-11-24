8d96ecfe807ea22897c5ad3cd8a3c47c14367678
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-11-24 14:18:25 +0200

    SR22AS-22 add classnames utility and img container

--- src/components/mediaCard/MediaCard.js
+++ src/components/mediaCard/MediaCard.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './media-card.scss';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 const MediaCard = ({ photo, altInfo, extended }) => {
+  const cardClass = classnames('media-card', {
+    'media-card--extended': extended,
+  });
   return (
-    <img
-      src={photo}
-      alt={altInfo}
-      className={'media-card' + (extended ? ' media-card--extended' : '')}
-    />
+    <div className={cardClass}>
+      <img src={photo} alt={altInfo} />
+    </div>
   );
 };
+//<img
+//   src={photo}
+//   alt={altInfo}
+//   className={'media-card' + (extended ? ' media-card--extended' : '')}
+// />
 
 MediaCard.propTypes = {
   photo: PropTypes.string.isRequired,
