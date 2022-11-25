b92ea0f23088d8a3a46acfd6033a69717eced283
Linas Paulauskas <paulauskas.linas@gmail.com>
2022-11-25 19:24:12 +0200

    SR22AS-20 add classnames utility and update css classes

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './card-container.scss';
+import classNames from 'classnames';
 
 export default function CardContainer({
   title,
-  alignment = 'center',
   theme = 'primary',
+  right = false,
+  center = false,
   children,
 }) {
+  const cardClass = classNames(
+    'card-container__title',
+    `card-container__title--${theme}`,
+    {
+      'card-container__title--right': right,
+      'card-container__title--center': center,
+    }
+  );
   return (
     <section className="card-container">
-      <h1
-        className={`card-container__title card-container__title--${alignment} card-container__title--${theme}`}
-      >
-        {title}
-      </h1>
+      <h1 className={cardClass}>{title}</h1>
       <div className="card-container__content">{children}</div>
     </section>
   );
@@ -23,6 +29,8 @@ export default function CardContainer({
 CardContainer.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
-  alignment: PropTypes.oneOf(['left', 'right', 'center']),
+  right: PropTypes.bool,
+  center: PropTypes.bool,
+  alignment: PropTypes.oneOf(['right', 'center']),
   theme: PropTypes.oneOf(['primary', 'secondary', 'tertiary', 'quaternary']),
 };
