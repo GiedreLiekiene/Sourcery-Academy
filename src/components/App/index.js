46da609160627c68d8ba8db9689d2d581d6353de
Auksely <auksely@gmail.com>
2022-11-30 11:36:38 +0200

    feat/SR22AS-39_routes create variable to store routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -11,22 +11,52 @@ import AcademyFrontEnd from './SourceryForFrontEnd';
 import AcademyKids from './SourceryForKids';
 import AcademyTesters from './SourceryForTesters';
 
+const routes = [
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/sourcery-for-developers',
+    element: <AcademyDevelopers />,
+  },
+  {
+    path: '/sourcery-for-testers',
+    element: <AcademyTesters />,
+  },
+  {
+    path: '/sourcery-for-front-end',
+    element: <AcademyFrontEnd />,
+  },
+  {
+    path: '/sourcery-for-kids',
+    element: <AcademyKids />,
+  },
+  {
+    path: '/media',
+    element: <Media />,
+  },
+  {
+    path: '/questions',
+    element: <Questions />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+];
+
 export default function App({ prop }) {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route
-          path="/sourcery-for-developers"
-          element={<AcademyDevelopers />}
-        />
-        <Route path="/sourcery-for-testers" element={<AcademyTesters />} />
-        <Route path="/sourcery-for-front-end" element={<AcademyFrontEnd />} />
-        <Route path="/sourcery-for-kids" element={<AcademyKids />} />
-        <Route path="/media" element={<Media />} />
-        <Route path="/questions" element={<Questions />} />
-        <Route path="/register" element={<Register />} />
+        {routes.map((route, i) => (
+          <Route key={i} path={route.path} element={route.element} />
+        ))}
       </Routes>
     </Router>
   );
