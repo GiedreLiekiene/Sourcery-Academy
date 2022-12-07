02bb41cd21d7e7fd84569ecf47ae2ab8980f3b5f
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-12-08 00:05:48 +0200

    SR22AS-29 add MediaContainer

--- src/components/MediaContainer/MediaContainer.jsx
+++ src/components/MediaContainer/MediaContainer.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import ErrorMessage from '../ErrorMessage/ErrorMessage.jsx';
 import MediaCard from '../MediaCard/MediaCard.js';
-import './media-card-container.scss';
+import './media-container.scss';
 import classnames from 'classnames';
 
 const mediaUrl = 'https://sfe-2022-data.netlify.app/static/media.json';
-function MediaCardContainer() {
+function MediaContainer() {
   const [error, setError] = useState(null);
   const [images, setImages] = useState(null);
 
@@ -32,11 +32,11 @@ function MediaCardContainer() {
   }
 
   return (
-    <div className="mediacard-container">
+    <div className="media-container">
       {images.map(({ thumbnail }, index) => {
         let extended = index == 1 || index == 5;
-        const cardClass = classnames('mediacard-container__item', {
-          'mediacard-container__item--extended': extended,
+        const cardClass = classnames('media-container__item', {
+          'media-container__item--extended': extended,
         });
         return (
           <div className={cardClass} key={thumbnail}>
@@ -48,4 +48,4 @@ function MediaCardContainer() {
   );
 }
 
-export default MediaCardContainer;
+export default MediaContainer;
