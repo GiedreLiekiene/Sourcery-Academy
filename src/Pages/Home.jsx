5b56540ef31bfaac5ab8dbea42c18ff6d6b3d3e2
Linas Paulauskas <paulauskas.linas@gmail.com>
2022-12-09 20:05:54 +0200

    SR22AS-25 Import svglist, styles, add containers for divs and images

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,9 +3,10 @@ import TextImgContainer from '../components/TextImgContainer/TextImgContainer';
 import Academies from '../components/Academies/Academies';
 import TextButtonContainer from '../components/TextButtonContainer/TextButtonContainer';
 import CardContainer from '../components/CardContainer/CardContainer';
-import TestimonialsContent from '../components/TestimonialsContent/TestimonialsContent';
 import academyInformation from '../utils/academyInformation.json';
 import { assets } from '../assets/svg/assets';
+import { SvgList } from '../assets/svg/index';
+import './home.scss';
 
 const {
   WalkingToLaptopImg,
@@ -15,6 +16,14 @@ const {
   MacWithHammer,
 } = assets;
 
+const { FirstWaveSvg, SecondWaveSvg, ThirdWaveSvg, FourthWaveSvg } = SvgList;
+
+function getAcademyInfo(key, academy) {
+  return academyInformation
+    .filter((item) => item.academy === academy)
+    .map((item) => item[key])
+    .toString();
+}
 function Home() {
   const [data, setData] = useState(academyInformation);
 
@@ -28,76 +37,84 @@ function Home() {
             text="Sourcery academy is our free of charge education program created to allow students to improve their skills and prepare for a career in the IT industry. There are three academies available. They serve as our long- term commitment to increase the number of students in Lithuania entering information technologies studies."
           />
         }
-        rightChild={<WalkingToLaptopImg />}
+        rightChild={
+          <>
+            <WalkingToLaptopImg />
+          </>
+        }
         alignCenter
       ></TextImgContainer>
+
       <Academies />
 
-      <TextImgContainer
-        leftChild={
-          <TextButtonContainer
-            level={2}
-            title="Sourcery for Developers"
-            buttonName="Learn more"
-            text={academyInformation
-              .filter((item) => item.academy === 'Sourcery for Developers')
-              .map((item) => item.intro)
-              .toString()}
-          />
-        }
-        rightChild={<WorkingWithLaptop />}
-        alignCenter
-      ></TextImgContainer>
+      <div className="section-group">
+        <TextImgContainer
+          leftChild={
+            <TextButtonContainer
+              headingLevel={2}
+              title="Sourcery for Developers"
+              buttonName="Learn more"
+              text={getAcademyInfo('intro', 'Sourcery for Developers')}
+            />
+          }
+          rightChild={<WorkingWithLaptop />}
+        ></TextImgContainer>
+        <div className="section-group__rightWave">
+          <FirstWaveSvg />
+        </div>
+      </div>
 
-      <TextImgContainer
-        leftChild={<MacWithHammer />}
-        rightChild={
-          <TextButtonContainer
-            level={2}
-            title="Sourcery for Testers"
-            alignRight
-            buttonName="Learn more"
-            text={academyInformation
-              .filter((item) => item.academy === 'Sourcery for Testers')
-              .map((item) => item.intro)
-              .toString()}
-          />
-        }
-        alignRight
-      ></TextImgContainer>
+      <div className="section-group">
+        <TextImgContainer
+          leftChild={<MacWithHammer />}
+          rightChild={
+            <TextButtonContainer
+              alignRight
+              headingLevel={2}
+              title="Sourcery for Testers"
+              buttonName="Learn more"
+              text={getAcademyInfo('intro', 'Sourcery for Testers')}
+            />
+          }
+          alignRight
+        ></TextImgContainer>
+        <div className="section-group__leftWave">
+          <SecondWaveSvg />
+        </div>
+      </div>
 
-      <TextImgContainer
-        leftChild={<WebTechAbbr />}
-        rightChild={
-          <TextButtonContainer
-            level={2}
-            title="Sourcery for Front-End"
-            alignRight
-            buttonName="Learn more"
-            text={academyInformation
-              .filter((item) => item.academy === 'Sourcery for Front-End')
-              .map((item) => item.intro)
-              .toString()}
-          />
-        }
-        alignRight
-      ></TextImgContainer>
+      <div className="section-group">
+        <TextImgContainer
+          leftChild={<WebTechAbbr />}
+          rightChild={
+            <TextButtonContainer
+              headingLevel={2}
+              title="Sourcery for Front-End"
+              alignRight
+              buttonName="Learn more"
+              text={getAcademyInfo('intro', 'Sourcery for Front-End')}
+            />
+          }
+          alignRight
+        ></TextImgContainer>
+      </div>
 
-      <TextImgContainer
-        leftChild={
-          <TextButtonContainer
-            level={2}
-            title="Sourcery for Kids"
-            buttonName="Learn more"
-            text={academyInformation
-              .filter((item) => item.academy === 'Sourcery for Kids')
-              .map((item) => item.intro)
-              .toString()}
-          />
-        }
-        rightChild={<WorkingAtDeskKidRobot />}
-      ></TextImgContainer>
-
+      <div className="section-group">
+        <TextImgContainer
+          leftChild={
+            <TextButtonContainer
+              headingLevel={2}
+              title="Sourcery for Kids"
+              buttonName="Learn more"
+              text={getAcademyInfo('intro', 'Sourcery for Kids')}
+            />
+          }
+          rightChild={<WorkingAtDeskKidRobot />}
+        ></TextImgContainer>
+        <div className="section-group__rightWave">
+          <ThirdWaveSvg />
+        </div>
+      </div>
       <CardContainer title="Testimonials" right>
         <div></div>
       </CardContainer>
