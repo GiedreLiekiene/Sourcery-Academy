56dfb304eaafbd39d3832da9c94b63a557a39739
Linas Paulauskas <paulauskas.linas@gmail.com>
2022-12-10 15:25:56 +0200

    SR22AS-25 Add class and styles file

--- src/components/ImageHolder/ImageHolder.jsx
+++ src/components/ImageHolder/ImageHolder.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function ImageHolder({ image }) {
-  return <>{image}</>;
+  return <div className="image-holder">{image}</div>;
 }
 
 ImageHolder.propTypes = {
