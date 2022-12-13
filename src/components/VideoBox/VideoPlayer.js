446d9a068c6772c29ac943cb4b8f28b777eadf04
PaulinaBaur <bve.paulina@gmail.com>
2022-12-13 21:06:42 +0200

    remove unused imports

--- src/components/VideoBox/VideoPlayer.js
+++ src/components/VideoBox/VideoPlayer.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 import './video-player.scss';
 
 function VideoPlayer({ videoSrc }) {
