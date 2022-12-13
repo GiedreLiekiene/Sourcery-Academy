f674027094d1befd14a7a13b0c0939911ff98aa7
PaulinaBaur <bve.paulina@gmail.com>
2022-12-13 21:00:21 +0200

    SR22AS-44 fix video player

--- src/components/VideoBox/VideoPlayer.js
+++ src/components/VideoBox/VideoPlayer.js
@@ -1,20 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import './video-player.scss';
 
 function VideoPlayer({ videoSrc }) {
   const videoRef = useRef(null);
-
-  useEffect(() => {
-    const video = videoRef.current;
-
-    video.addEventListener('play', () => {
-      console.log('The video is playing');
-    });
-    video.addEventListener('pause', () => {
-      console.log('The video is paused');
-    });
-  });
 
   return (
     <video ref={videoRef} src={videoSrc} controls className="video-player" />
