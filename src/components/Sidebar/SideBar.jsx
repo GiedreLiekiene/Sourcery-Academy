7dd03355c2d2c7f57e0f541d889aa465f8380e78
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-12-15 23:22:57 +0200

    SR22AS-33 add overlay functionality to mobile menu

--- src/components/Sidebar/SideBar.jsx
+++ src/components/Sidebar/SideBar.jsx
@@ -7,25 +7,28 @@ import PropTypes from 'prop-types';
 const Sidebar = ({ links, close }) => {
   const location = useLocation();
   return (
-    <div className="sidebar" onClick={close}>
-      <Link to="/" className="sidebar__logo">
-        <LogoSvg />
-      </Link>
-      {links.map(({ path, name, onClick }) => (
-        <Link
-          to={path}
-          className={
-            location.pathname === path
-              ? 'sidebar__link active'
-              : 'sidebar__link'
-          }
-          key={name}
-          onClick={onClick}
-        >
-          {name}
+    <>
+      <div className="overlay" onClick={close} />
+      <div className="sidebar" onClick={close}>
+        <Link to="/" className="sidebar__logo">
+          <LogoSvg />
         </Link>
-      ))}
-    </div>
+        {links.map(({ path, name, onClick }) => (
+          <Link
+            to={path}
+            className={
+              location.pathname === path
+                ? 'sidebar__link active'
+                : 'sidebar__link'
+            }
+            key={name}
+            onClick={onClick}
+          >
+            {name}
+          </Link>
+        ))}
+      </div>
+    </>
   );
 };
 
