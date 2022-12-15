ce057add57cf8172fc83e153973c96d9e0ef0c69
Auksely <auksely@gmail.com>
2022-12-15 21:01:21 +0200

    SR22AS-23_modal change widths

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,11 +2,12 @@ import React,{useEffect} from "react";
 import ReactDOM from "react-dom";
 import './modal.scss';
 import PropTypes from "prop-types";
+import classNames from "classnames";
 
-const Modal =({open, close, children })=>{
+const Modal =({ open, close, children, min, max })=>{
 
   const closeOnEsc = (e) =>{
-    if((e.charCode||e.keyCode)===27){
+    if((e.charCode||e.keyCode) === 27){
       close();
     }
   }
@@ -18,12 +19,18 @@ const Modal =({open, close, children })=>{
     }
   },[])
 
+  const modalClass=classNames(
+    'modal-container__content',
+    {'modal-container__content--min': min,
+      'modal-container__content--max': max,
+    }
+  );
 
   return (
     open? ReactDOM.createPortal(
-      <div>
-      <button className="modal__overlay" onClick={close}></button>
-      <div className = "modal" >
+      <div className="modal-container">
+      <button className="modal-container__overlay" onClick={close}></button>
+      <div className={modalClass}>
           {children}
       </div>
       </div>,document.body):null
@@ -36,4 +43,6 @@ Modal.propTypes = {
   children: PropTypes.node.isRequired,
   open: PropTypes.bool,
   close: PropTypes.func,
+  min: PropTypes.bool,
+  max: PropTypes.bool,
 };
