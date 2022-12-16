8817aefd1e9211f2293fb667864f157fb4d12615
Auksely <auksely@gmail.com>
2022-12-16 16:31:43 +0200

    SR22AS-23_modal run prettier fix, add media

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,43 +1,40 @@
-import React,{useEffect} from "react";
-import ReactDOM from "react-dom";
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import './modal.scss';
-import PropTypes from "prop-types";
-import classNames from "classnames";
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-const Modal =({ open, close, children, min, max })=>{
-
-  const closeOnEsc = (e) =>{
-    if((e.charCode||e.keyCode) === 27){
+const Modal = ({ open, close, children, min, max }) => {
+  const closeOnEsc = (e) => {
+    if ((e.charCode || e.keyCode) === 27) {
       close();
     }
-  }
+  };
 
-  useEffect(()=>{
-    document.body.addEventListener('keydown',closeOnEsc);
-    return function cleanup(){
-      document.body.removeEventListener('keydown',closeOnEsc)
-    }
-  },[])
+  useEffect(() => {
+    document.body.addEventListener('keydown', closeOnEsc);
+    return function cleanup() {
+      document.body.removeEventListener('keydown', closeOnEsc);
+    };
+  }, []);
 
-  const modalClass=classNames(
-    'modal-container__content',
-    {'modal-container__content--min': min,
-      'modal-container__content--max': max,
-    }
-  );
+  const modalClass = classNames('modal-container__content', {
+    'modal-container__content--min': min,
+    'modal-container__content--max': max,
+  });
 
-  return (
-    open? ReactDOM.createPortal(
-      <div className="modal-container">
-      <button className="modal-container__overlay" onClick={close}></button>
-      <div className={modalClass}>
-          {children}
-      </div>
-      </div>,document.body):null
-  );
-}
+  return open
+    ? ReactDOM.createPortal(
+        <div className="modal-container">
+          <button className="modal-container__overlay" onClick={close}></button>
+          <div className={modalClass}>{children}</div>
+        </div>,
+        document.body
+      )
+    : null;
+};
 
-export default Modal
+export default Modal;
 
 Modal.propTypes = {
   children: PropTypes.node.isRequired,
