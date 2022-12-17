2042b5836f160a3cfa0eedc9e068117a5d6589a5
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-12-17 18:03:59 +0200

    SR22AS-33 add classnames & smaccs + modifier class

--- src/components/SidebarButton/SidebarButton.jsx
+++ src/components/SidebarButton/SidebarButton.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './sidebar-button.scss';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 const SidebarButton = ({ onClick, active }) => {
+  const buttonClass = classNames('sidebar-button', {
+    'sidebar-button is-active': active,
+  });
   return (
-    <button
-      onClick={onClick}
-      className={active ? 'sidebar-button active' : 'sidebar-button'}
-    >
-      <div className="sidebar-button--bar" />
-      <div className="sidebar-button--bar" />
-      <div className="sidebar-button--bar" />
+    <button onClick={onClick} className={buttonClass}>
+      <div className="sidebar-button__bar sidebar-button__bar--top" />
+      <div className="sidebar-button__bar sidebar-button__bar--middle" />
+      <div className="sidebar-button__bar sidebar-button__bar--bottom" />
     </button>
   );
 };
