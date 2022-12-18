b52550cad5862dfe040827eecf8645010a3f8465
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-12-18 02:27:34 +0200

    SR22AS-33 add Academies dropdown submenu to mobile

--- src/components/Mobilemenu/Mobilemenu.jsx
+++ src/components/Mobilemenu/Mobilemenu.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import './mobile-menu.scss';
 import { Link, useLocation } from 'react-router-dom';
 import LogoSvg from '~/assets/svg/Logo.svg';
 import PropTypes from 'prop-types';
+import Dropdown from '~/components/navbar/Dropdown.js';
 
 const Mobilemenu = ({ links, close }) => {
   const location = useLocation();
+  const [dropdown, setDropdown] = useState(false);
   return (
     <>
       <div className="overlay" onClick={close} />
@@ -13,20 +15,35 @@ const Mobilemenu = ({ links, close }) => {
         <Link to="/" className="mobile-menu__logo">
           <LogoSvg />
         </Link>
-        {links.map(({ path, name, onClick }) => (
-          <Link
-            to={path}
-            className={
-              location.pathname === path
-                ? 'mobile-menu__link active'
-                : 'mobile-menu__link'
-            }
-            key={name}
-            onClick={onClick}
-          >
-            {name}
-          </Link>
-        ))}
+        {links.map(({ path, name, Icon, submenu }) => {
+          let openDropdown = (e) => {
+            setDropdown((prev) => !prev);
+            e.stopPropagation();
+          };
+          return (
+            <Fragment key={name}>
+              <Link
+                to={path}
+                className={
+                  location.pathname === path
+                    ? 'mobile-menu__link active'
+                    : 'mobile-menu__link'
+                }
+                key={name}
+                aria-expanded={dropdown ? 'true' : 'false'}
+                onClick={submenu ? openDropdown : undefined}
+              >
+                {name}
+                {Icon && <Icon className="mobile-menu__link-icon" />}
+              </Link>
+              {submenu && dropdown && (
+                <div className="submenu">
+                  <Dropdown submenuItems={submenu} dropdown={dropdown} />
+                </div>
+              )}
+            </Fragment>
+          );
+        })}
       </div>
     </>
   );
