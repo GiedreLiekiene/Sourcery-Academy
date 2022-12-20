f15af2c2925765f835896ce91081d3555f10d836
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-12-20 02:48:06 +0200

    SR22AS-33 add mobileMenuClass function

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-
 import LogoSvg from '~/assets/svg/Logo.svg';
 import './navbar.scss';
 import NavbarLinks from './NavbarLinks';
 import { ROUTES } from '../App/Routes';
-import Mobilemenu from '../Mobilemenu/Mobilemenu.jsx';
 import MobilemenuButton from '../MobilemenuButton/MobilemenuButton';
+import PropTypes from 'prop-types';
 
 const Navbar = () => {
   const [showMobilemenu, setShowMobilemenu] = useState(false);
@@ -16,23 +15,38 @@ const Navbar = () => {
   function closeMobilemenu() {
     setShowMobilemenu(false);
   }
+  let mobileMenuClass = showMobilemenu ? '' : ' hide';
   return (
     <>
       <div className="navbar">
         <Link to={ROUTES.home} className="navbar__logo">
           <LogoSvg />
         </Link>
-        <div className="navbar__links">
-          <NavbarLinks popupSubmenu={true} />
+        <div
+          className={'overlay' + mobileMenuClass}
+          onClick={closeMobilemenu}
+        />
+        <div
+          className={'navbar__links' + mobileMenuClass}
+          onClick={closeMobilemenu}
+        >
+          <Link to={ROUTES.home} className="navbar__links__logo">
+            <LogoSvg />
+          </Link>
+          <NavbarLinks />
         </div>
+
         <MobilemenuButton
           onClick={() => setShowMobilemenu(!showMobilemenu)}
           active={showMobilemenu}
         />
       </div>
-      {showMobilemenu && <Mobilemenu close={closeMobilemenu} />}
     </>
   );
 };
 
 export default Navbar;
+
+Navbar.propTypes = {
+  close: PropTypes.func,
+};
