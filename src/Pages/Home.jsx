2579cba9a3595219d0429079bf4398f0ec7477d4
Linas Paulauskas <paulauskas.linas@gmail.com>
2022-12-28 12:02:25 +0200

    SR22AS-52 Wrap Home page with ThemeProvider

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,6 +9,7 @@ import ImageHolder from '../components/ImageHolder/ImageHolder';
 import academyInformation from '../utils/academyInformation.json';
 import { assets } from '../assets/svg/assets';
 import { SvgList } from '../assets/svg/index';
+import { ThemeProvider } from '../utils/ThemeContext';
 import './home.scss';
 
 const {
@@ -30,100 +31,102 @@ function getAcademyInfo(key, academy) {
 function Home() {
   return (
     <>
-      <div className="section-group">
-        <TextImgContainer
-          leftChild={
-            <TextActionContainer
-              title="Sourcery Academy"
-              text="Sourcery academy is our free of charge education program created to allow students to improve their skills and prepare for a career in the IT industry. There are three academies available. They serve as our long- term commitment to increase the number of students in Lithuania entering information technologies studies."
-            />
-          }
-          rightChild={<WalkingToLaptopImg />}
-          alignCenter
-        ></TextImgContainer>
-      </div>
-
-      <Academies />
-
-      <div className="section-group">
-        <TextImgContainer
-          leftChild={
-            <TextActionContainer
-              headingLevel={2}
-              title="Sourcery for Developers"
-              text={getAcademyInfo('intro', 'Sourcery for Developers')}
-            />
-          }
-          rightChild={<ImageHolder image={<WorkingWithLaptop />} />}
-        ></TextImgContainer>
-        <div className="section-group__first-wave">
-          <FirstWaveSvg />
+      <ThemeProvider theme="primary">
+        <div className="section-group">
+          <TextImgContainer
+            leftChild={
+              <TextActionContainer
+                title="Sourcery Academy"
+                text="Sourcery academy is our free of charge education program created to allow students to improve their skills and prepare for a career in the IT industry. There are three academies available. They serve as our long- term commitment to increase the number of students in Lithuania entering information technologies studies."
+              />
+            }
+            rightChild={<WalkingToLaptopImg />}
+            alignCenter
+          ></TextImgContainer>
         </div>
-      </div>
 
-      <div className="section-group">
-        <TextImgContainer
-          leftChild={<ImageHolder right image={<MacWithHammer />} />}
-          rightChild={
-            <TextActionContainer
-              alignRight
-              headingLevel={2}
-              title="Sourcery for Testers"
-              text={getAcademyInfo('intro', 'Sourcery for Testers')}
-            />
-          }
-          alignRight
-        ></TextImgContainer>
-        <div className="section-group__second-wave">
-          <SecondWaveSvg />
+        <Academies />
+
+        <div className="section-group">
+          <TextImgContainer
+            leftChild={
+              <TextActionContainer
+                headingLevel={2}
+                title="Sourcery for Developers"
+                text={getAcademyInfo('intro', 'Sourcery for Developers')}
+              />
+            }
+            rightChild={<ImageHolder image={<WorkingWithLaptop />} />}
+          ></TextImgContainer>
+          <div className="section-group__first-wave">
+            <FirstWaveSvg />
+          </div>
         </div>
-      </div>
 
-      <div className="section-group">
-        <TextImgContainer
-          leftChild={<ImageHolder right image={<WebTechAbbr />} />}
-          rightChild={
-            <TextActionContainer
-              headingLevel={2}
-              title="Sourcery for Front-End"
-              alignRight
-              text={getAcademyInfo('intro', 'Sourcery for Front-End')}
-            />
-          }
-          alignRight
-        ></TextImgContainer>
-      </div>
-
-      <div className="section-group">
-        <TextImgContainer
-          leftChild={
-            <TextActionContainer
-              headingLevel={2}
-              title="Sourcery for Kids"
-              text={getAcademyInfo('intro', 'Sourcery for Kids')}
-            />
-          }
-          rightChild={<ImageHolder image={<WorkingAtDeskKidRobot />} />}
-        ></TextImgContainer>
-        <div className="section-group__third-wave">
-          <ThirdWaveSvg />
+        <div className="section-group">
+          <TextImgContainer
+            leftChild={<ImageHolder right image={<MacWithHammer />} />}
+            rightChild={
+              <TextActionContainer
+                alignRight
+                headingLevel={2}
+                title="Sourcery for Testers"
+                text={getAcademyInfo('intro', 'Sourcery for Testers')}
+              />
+            }
+            alignRight
+          ></TextImgContainer>
+          <div className="section-group__second-wave">
+            <SecondWaveSvg />
+          </div>
         </div>
-      </div>
 
-      <div className="section-group">
-        <CardContainer title="Testimonials" right>
-          <TestimonialsContainer academy="Random" />
-        </CardContainer>
-      </div>
-
-      <div className="section-group">
-        <CardContainer title="Media">
-          <MediaContainer />
-        </CardContainer>
-        <div className="section-group__fourth-wave">
-          <FourthWaveSvg />
+        <div className="section-group">
+          <TextImgContainer
+            leftChild={<ImageHolder right image={<WebTechAbbr />} />}
+            rightChild={
+              <TextActionContainer
+                headingLevel={2}
+                title="Sourcery for Front-End"
+                alignRight
+                text={getAcademyInfo('intro', 'Sourcery for Front-End')}
+              />
+            }
+            alignRight
+          ></TextImgContainer>
         </div>
-      </div>
+
+        <div className="section-group">
+          <TextImgContainer
+            leftChild={
+              <TextActionContainer
+                headingLevel={2}
+                title="Sourcery for Kids"
+                text={getAcademyInfo('intro', 'Sourcery for Kids')}
+              />
+            }
+            rightChild={<ImageHolder image={<WorkingAtDeskKidRobot />} />}
+          ></TextImgContainer>
+          <div className="section-group__third-wave">
+            <ThirdWaveSvg />
+          </div>
+        </div>
+
+        <div className="section-group">
+          <CardContainer title="Testimonials" right>
+            <TestimonialsContainer academy="Random" />
+          </CardContainer>
+        </div>
+
+        <div className="section-group">
+          <CardContainer title="Media">
+            <MediaContainer />
+          </CardContainer>
+          <div className="section-group__fourth-wave">
+            <FourthWaveSvg />
+          </div>
+        </div>
+      </ThemeProvider>
     </>
   );
 }
