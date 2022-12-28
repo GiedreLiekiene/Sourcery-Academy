dc78f69ec3f528b15a5e0e85b62364c13848e43b
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-12-28 14:48:43 +0200

    SR22AS-67 add forceWrapTitle prop

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -36,6 +36,7 @@ function Home() {
             <TextActionContainer
               title="Sourcery Academy"
               text="Sourcery academy is our free of charge education program created to allow students to improve their skills and prepare for a career in the IT industry. There are three academies available. They serve as our long- term commitment to increase the number of students in Lithuania entering information technologies studies."
+              forceWrapTitle={true}
             />
           }
           rightChild={<WalkingToLaptopImg />}
