a9387a0c4eab09e49208f7cc8283e05c35d8b803
Linas Paulauskas <paulauskas.linas@gmail.com>
2022-12-28 11:53:36 +0200

    SR22AS-52 Add themecontext, boxshadow compononent. Convert header and panel into components

--- src/components/ScheduleCard/ScheduleCard.jsx
+++ src/components/ScheduleCard/ScheduleCard.jsx
@@ -1,41 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
-import ArrowSVG from '~/assets/svg/navbar-icon.svg';
 import './schedule-card.scss';
-
-function ScheduleCard({ id, title, children, theme }) {
+import ScheduleCardHeader from './ScheduleCardHeader';
+import ScheduleCardPanel from './ScheduleCardPanel';
+import BoxShadow from '../BoxShadow/BoxShadow';
+import { ThemeContext } from '../../utils/ThemeContext';
+function ScheduleCard({
+  scheduleInfo: { id, title, lecturers, duration, startTime, sessions },
+}) {
   const [isExpanded, setIsExpanded] = useState(false);
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div className="schedule-card">
-      <h3 className="schedule-card__header">
-        <button
-          id={`${id}-header`}
-          aria-controls={`${id}-panel`}
-          aria-expanded={isExpanded}
-          onClick={() => setIsExpanded(!isExpanded)}
-        >
-          {title}
-          <ArrowSVG />
-        </button>
-      </h3>
-      {isExpanded && (
-        <section
-          id={`${id}-panel`}
-          aria-labelledby={`${id}-header`}
-          hidden={!isExpanded}
-        >
-          {children}
-        </section>
-      )}
-    </div>
+    <BoxShadow theme={theme}>
+      <div className="schedule-card">
+        <ScheduleCardHeader
+          id={id}
+          title={title}
+          isExpanded={isExpanded}
+          setIsExpanded={setIsExpanded}
+        />
+
+        {isExpanded && (
+          <ScheduleCardPanel
+            id={id}
+            lecturers={lecturers}
+            startTime={startTime}
+            duration={duration}
+            isExpanded={isExpanded}
+            sessions={sessions}
+          />
+        )}
+      </div>
+    </BoxShadow>
   );
 }
 
 ScheduleCard.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired,
-  id: PropTypes.number,
-  theme: PropTypes.oneOf(['primary', 'secondary', 'tertiary', 'quaternary']),
+  scheduleInfo:
+    PropTypes.object &&
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      lecturers: PropTypes.string.isRequired,
+      startTime: PropTypes.string.isRequired,
+      duration: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
+      sessions: PropTypes.arrayOf(PropTypes.object),
+    }),
 };
 
 export default ScheduleCard;
