d5dd26131c5c79d2614724e4fcfe6eaaabdbcd7a
Linas Paulauskas <paulauskas.linas@gmail.com>
2022-12-28 11:49:29 +0200

    SR22AS-77 Add themes and change variables to scss variables

--- src/components/BoxShadow/BoxShadow.jsx
+++ src/components/BoxShadow/BoxShadow.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './box-shadow.scss';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-const BoxShadow = ({ children }) => {
-  return <div className="box-shadow">{children}</div>;
+const BoxShadow = ({ children, theme = 'primary' }) => {
+  const boxClass = classNames('box-shadow', `box-shadow--${theme}`);
+  return <div className={boxClass}>{children}</div>;
 };
 
 BoxShadow.propTypes = {
   children: PropTypes.node.isRequired,
+  theme: PropTypes.oneOf(['primary', 'secondary', 'tertiary', 'quaternary']),
 };
 
 export default BoxShadow;
