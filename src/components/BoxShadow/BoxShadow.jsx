b6358977d8eaaa9e854ac0eec902bd9ba81cc82d
Linas Paulauskas <paulauskas.linas@gmail.com>
2022-12-29 12:10:58 +0200

    SR22AS-52 Add ThemeContext to BoxShadow component

--- src/components/BoxShadow/BoxShadow.jsx
+++ src/components/BoxShadow/BoxShadow.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './box-shadow.scss';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { ThemeContext } from '../../utils/ThemeContext';
 
-const BoxShadow = ({ children, theme = 'primary' }) => {
+const BoxShadow = ({ children }) => {
+  const { theme } = useContext(ThemeContext);
   const boxClass = classNames('box-shadow', `box-shadow--${theme}`);
   return <div className={boxClass}>{children}</div>;
 };
 
 BoxShadow.propTypes = {
   children: PropTypes.node.isRequired,
-  theme: PropTypes.oneOf(['primary', 'secondary', 'tertiary', 'quaternary']),
 };
 
 export default BoxShadow;
