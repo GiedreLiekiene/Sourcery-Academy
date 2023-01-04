eb990e026d3f8013f101ff7469ec018a36ff6364
Auksely <auksely@gmail.com>
2023-01-04 21:03:38 +0200

    SR22AS-84_create-checkbox create check box

--- src/components/CheckBox/CheckBox.jsx
+++ src/components/CheckBox/CheckBox.jsx
@@ -3,35 +3,29 @@ import './check-box.scss';
 import PropTypes from 'prop-types';
 const Checkbox = ({
   type = 'checkbox',
-  id = 'checkbox',
   label,
   isChecked,
   value,
   name,
   onChange,
-  tabIndex = 0,
 }) => {
   return (
     <div className="checkbox">
       <input
-        id={id}
-        className="checkbox__box"
+        className="checkbox__check"
         type={type}
         checked={isChecked}
         value={value}
         name={name}
         onChange={onChange}
-        tabIndex={tabIndex}
+        tabIndex={0}
       />
-      <label htmlFor="checkbox" className="checkbox__label">
-        {label}
-      </label>
+      <label className="checkbox__label">{label}</label>
     </div>
   );
 };
 export default Checkbox;
 Checkbox.propTypes = {
-  id: PropTypes.string,
   type: PropTypes.string,
   label: PropTypes.string.isRequired,
   isChecked: PropTypes.node.isRequired,
