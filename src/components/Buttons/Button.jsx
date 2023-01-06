49cff734cb37c14ae0603d2e30f0872aeb1ed8fc
Auksely <auksely@gmail.com>
2023-01-06 13:29:26 +0200

    SR22AS-82_create-button

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './button.scss';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-const Button = ({ type = 'button', children, onClick }) => {
+const Button = ({ type = 'submit', children, onClick, theme = 'primary' }) => {
+  const buttonClass = classNames('button', `button--${theme}`);
   return (
-    <button type={type} className="button" onClick={onClick}>
+    <button type={type} className={buttonClass} onClick={onClick}>
       {children}
     </button>
   );
 };
 export default Button;
 Button.propTypes = {
+  theme: PropTypes.oneOf(['primary', 'secondary', 'tertiary', 'quaternary']),
   type: PropTypes.string,
   children: PropTypes.string.isRequired,
   onClick: PropTypes.node.isRequired,
