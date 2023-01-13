4065bb6befc4815d67d1fe6086dcdec1259ff4ca
Giedre Liekiene <giedre.valanciute@gmail.com>
2023-01-13 19:02:30 +0200

    SR22AS-99 create custom hook for sml screens Carou

---
+++ src/components/MediaContainer/useIsWindowWiderThan.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+function getWindowWidth() {
+  return window.innerWidth;
+}
+
+export default function useIsWindowWiderThan(width) {
+  const [isWider, setIsWider] = useState(getWindowWidth() > width);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsWider(getWindowWidth() > width);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [width]);
+
+  return isWider;
+}
