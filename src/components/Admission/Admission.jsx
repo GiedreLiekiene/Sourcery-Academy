221b71fea55b3d374a1891ffb91430f8f4520780
Arturito <arturaskisielius91@gmail.com>
2023-01-16 23:27:42 +0200

    layout, mobile view

--- src/components/Admission/Admission.jsx
+++ src/components/Admission/Admission.jsx
@@ -1,59 +1,56 @@
 import React, { useContext, useMemo } from 'react';
 import PropTypes from 'prop-types';
-import { ThemeContext } from '../../utils/ThemeContext';
 import './admission.scss';
 import CardContainer from '../CardContainer/CardContainer';
 import TextActionContainer from '../TextActionContainer/TextActionContainer';
 import TextImgContainer from '../TextImgContainer/TextImgContainer';
 import ImageHolder from '../ImageHolder/ImageHolder';
 import ShowingLocations from '../../assets/svg/image-girl-showing-locations.svg';
-import Dates from '../../assets/svg/Dates.svg';
 import academyInformation from '../../utils/academyInformation.json';
 
-export default function Admission({ academy = 0 }) {
-  const { theme } = useContext(ThemeContext);
-  const admissionIntroduction =
-    academyInformation[academy].admission.introduction;
-  const admissionPoints =
-    academyInformation[academy].admission['learning points'];
-  const admissionInterview =
-    academyInformation[academy].admission['interview promise'];
-
-  const learningPoints = useMemo(() =>
-    admissionPoints.map((item, index) => (
-      <li className={`learning-point--${theme}`} key={index}>
-        {item}
-      </li>
-    ))
-  );
+export default function Admission({ academy = 'Sourcery for Developers' }) {
+  const getApplicationSteps = (academy) => {
+    return academyInformation
+      .filter((item) => item.academy === academy)
+      .map((item) => item.admission)[0];
+  };
+  const applicationSteps = useMemo(() => getApplicationSteps(academy), []);
 
   return (
     <div className="admission-wrapper">
       <CardContainer title="The Admission">
         <TextImgContainer
           leftChild={
-            <div className="admission-wrapper__text-block">
+            <div>
               <TextActionContainer
                 headingLevel={2}
                 title="Introduction"
-                text={admissionIntroduction}
+                text={applicationSteps.introduction}
               />
               <TextActionContainer
                 className="points"
                 headingLevel={2}
                 title="Learning points"
-                text={learningPoints}
+                text={applicationSteps['learning points'].map(
+                  (point, index) => (
+                    <li
+                      className="admission-wrapper__point--secondary"
+                      key={index}
+                    >
+                      {point}
+                    </li>
+                  )
+                )}
               />
               <TextActionContainer
                 headingLevel={2}
                 title="Interview"
-                text={admissionInterview}
+                text={applicationSteps['interview promise']}
               />
             </div>
           }
           rightChild={
             <div>
-              <ImageHolder className="dates" right image={<Dates />} />
               <ImageHolder right image={<ShowingLocations />} />
             </div>
           }
@@ -64,5 +61,5 @@ export default function Admission({ academy = 0 }) {
 }
 
 Admission.propTypes = {
-  academy: PropTypes.number.isRequired,
+  academy: PropTypes.string.isRequired,
 };
