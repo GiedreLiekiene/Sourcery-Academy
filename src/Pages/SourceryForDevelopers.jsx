85578c27249b90d3e83c1f932694f7be0d3ab5f9
PaulinaBaur <bve.paulina@gmail.com>
2023-01-17 13:43:07 +0200

    SR22AS-8 add academies.scss

--- src/Pages/SourceryForDevelopers.jsx
+++ src/Pages/SourceryForDevelopers.jsx
@@ -13,8 +13,9 @@ import { ThemeProvider } from '../utils/ThemeContext';
 import { ROUTES } from '../components/App/Routes';
 import { SvgList } from '../assets/svg';
 import { assets } from '../assets/svg/assets';
+import './academies.scss';
 
-const { home } = ROUTES;
+const { register } = ROUTES;
 
 const { ApplyDevelopers } = assets;
 
@@ -29,21 +30,19 @@ function AcademyDevelopers() {
                 title="Sourcery Academy for developers"
                 text="Become a software developer with our free of charge three-month academy, available this fall and spring. Work with our top specialists as your mentors and lecturers in Kaunas and Vilnius."
               >
-                <div className='action-wrap'>
-                  <CustomLink to={home}>Register</CustomLink>
-                </div>
+                <CustomLink to={register}>Register</CustomLink>
               </TextActionContainer>
             }
             rightChild={
               <ImageHolder right image={<VideoBox thumbnail='https://sfe-2022-data.netlify.app/static/video/developers/thumbnails/47110170574_c00cc2b54d_k-1.jpg' videoSrc='https://sfe-2022-data.netlify.app/static/video/developers/80b0058a9428314582a3f25f3b1dfb8bc27de8a4.mp4' />} /> 
            }
           ></TextImgContainer>
-          <div className="section-group__register-wave">
+          <div className="section-group__wave section-group__wave--register-wave">
             <WaveContainer>            
               <SvgList.RegisterWaveSvg />
             </WaveContainer>
           </div>
-          <div className="section-group__video-wave">
+          <div className="section-group__wave section-group__wave--video-wave">
           <WaveContainer>
             <SvgList.VideoWaveSvg />
           </WaveContainer>
@@ -54,11 +53,10 @@ function AcademyDevelopers() {
         <CardContainer title="Schedule" center>
           <ScheduleContainer academy="Sourcery for Developers" />
         </CardContainer>
-        <div className="section-group__schedule-wave">
+        <div className="section-group__wave section-group__wave--schedule-wave">
           <WaveContainer>
             <SvgList.ScheduleWaveSvg />
           </WaveContainer>
-
         </div>
       </div>
 
@@ -72,7 +70,7 @@ function AcademyDevelopers() {
         <CardContainer title="Media">
           <MediaContainer academy={'developers'} shouldUseExtended={true} />
         </CardContainer>
-        <div className="section-group__media-wave">
+        <div className="section-group__wave section-group__wave--media-wave">
            <WaveContainer>
             <SvgList.MediaWaveSvg />
            </WaveContainer>
@@ -88,15 +86,13 @@ function AcademyDevelopers() {
               text="Entrance test results define a priority list of candidates. We accept 20 candidates with highest marks into academy."
               boldMainText
             >                
-            <div className='action-wrap'>
-                <CustomLink to={home}>Apply now</CustomLink>
-            </div>
+                <CustomLink to={register}>Apply now</CustomLink>
             </TextActionContainer>
           }
           rightChild={<ApplyDevelopers />}
           alignCenter
         ></TextImgContainer>
-        <div className="section-group__apply-wave">
+        <div className="section-group__wave section-group__wave--apply-wave">
           <WaveContainer>
             <SvgList.ApplyWaveSvg />
           </WaveContainer>
